ccc383512879b284c82bc25a0dc8655894a88bd8
Ole Krüger <ole@vprsm.de>
2022-03-20 18:44:58 +0000

    Organize test file better

--- src/adts/tests/test_adts.ml
+++ src/adts/tests/test_adts.ml
@@ -1,70 +1,17 @@
 open Alcotest
 open Adts
 
-module type Flavoured = sig
-  val flavour : string
-end
-
-let flavoured_test_case (module F : Flavoured) name =
-  test_case (Printf.sprintf "%s (%s)" name F.flavour) `Quick
-;;
-
-let flavoured_test_case_2 (module F : Flavoured) (module G : Flavoured) name =
-  test_case (Printf.sprintf "%s (%s, %s)" name F.flavour G.flavour) `Quick
-;;
-
-module type IntProduct = sig
-  include Product.S with type 'a wrapper = int
-
-  include Flavoured
-end
-
-let product_makers : (string * (module Product.Maker)) list =
-  [ "normal", (module Product.Make); "compact", (module Product.MakeCompact) ]
-;;
-
-let int_product_tests (f : (module IntProduct) -> 'a) =
-  let module W =
-    Wrappers.Const (struct
+let test_product_select
+    (module MakeProduct : Product.Maker)
+    (module MakeSum : Sum.Maker)
+    ()
+  =
+  let module P =
+    MakeProduct (Wrappers.Const (struct
       type t = int
-    end)
+    end))
   in
-  List.map
-    (fun (flavour, (module Maker : Product.Maker)) ->
-      let module P = struct
-        include Maker (W)
-
-        let flavour = flavour
-      end
-      in
-      f (module P))
-    product_makers
-;;
-
-module type UnitSum = sig
-  include Sum.S with type 'a wrapper = unit
-
-  include Flavoured
-end
-
-let sum_makers : (string * (module Sum.Maker)) list =
-  [ "normal", (module Sum.Make); "compact", (module Sum.MakeCompact) ]
-;;
-
-let unit_sum_tests (f : (module UnitSum) -> 'a) =
-  List.map
-    (fun (flavour, (module Maker : Sum.Maker)) ->
-      let module S = struct
-        include Maker (Wrappers.Unit)
-
-        let flavour = flavour
-      end
-      in
-      f (module S))
-    sum_makers
-;;
-
-let test_product_select (module P : IntProduct) (module S : UnitSum) () =
+  let module S = MakeSum (Wrappers.Unit) in
   let module Select = P.MakeSelect (S) in
   let handler : int Select.handler =
     let run : type x. x S.wrapper -> x P.wrapper -> int = fun () i -> i in
@@ -79,7 +26,12 @@ let test_product_select (module P : IntProduct) (module S : UnitSum) () =
   check int "must be equal" 73 value
 ;;
 
-let test_product_fold (module P : IntProduct) () =
+let test_product_fold (module Make : Product.Maker) () =
+  let module P =
+    Make (Wrappers.Const (struct
+      type t = int
+    end))
+  in
   let value = P.(13 ** 37 ** 73 ** 31 ** nil) in
   let rec folder : type xs. int -> (xs, int) P.folder =
    fun accum ->
@@ -93,7 +45,8 @@ let test_product_fold (module P : IntProduct) () =
   check int "sums must be equal" 154 sum
 ;;
 
-let test_sum_tag (module S : UnitSum) () =
+let test_sum_tag (module Make : Sum.Maker) () =
+  let module S = Make (Wrappers.Unit) in
   let sum = S.zero () in
   check int "tag zero must be 0" 0 (S.tag sum);
   let sum = S.succ sum in
@@ -102,30 +55,97 @@ let test_sum_tag (module S : UnitSum) () =
   check int "tag succ succ must be 2" 2 (S.tag sum)
 ;;
 
+(**************************************************************************************************)
+
+module type Flavoured = sig
+  val flavour : string
+end
+
+let flavoured_test_case (module F : Flavoured) name =
+  test_case (Printf.sprintf "%s (%s)" name F.flavour) `Quick
+;;
+
+let flavoured_test_case_2 (module F : Flavoured) (module G : Flavoured) name =
+  test_case (Printf.sprintf "%s (%s, %s)" name F.flavour G.flavour) `Quick
+;;
+
+let product_makers : (string * (module Product.Maker)) list =
+  [ "normal", (module Product.Make); "compact", (module Product.MakeCompact) ]
+;;
+
+module type MakeProduct = sig
+  module Make : Product.Maker
+
+  include Flavoured
+end
+
+let product_tests (f : (module MakeProduct) -> 'a) =
+  let module W =
+    Wrappers.Const (struct
+      type t = int
+    end)
+  in
+  List.map
+    (fun (flavour, (module Make : Product.Maker)) ->
+      let module P = struct
+        module Make = Make
+
+        let flavour = flavour
+      end
+      in
+      f (module P))
+    product_makers
+;;
+
+let sum_makers : (string * (module Sum.Maker)) list =
+  [ "normal", (module Sum.Make); "compact", (module Sum.MakeCompact) ]
+;;
+
+module type MakeSum = sig
+  module Make : Sum.Maker
+
+  include Flavoured
+end
+
+let sum_tests (f : (module MakeSum) -> 'a) =
+  List.map
+    (fun (flavour, (module Make : Sum.Maker)) ->
+      let module S = struct
+        module Make = Make
+
+        let flavour = flavour
+      end
+      in
+      f (module S))
+    sum_makers
+;;
+
+(**************************************************************************************************)
+
 let product_select_tests =
   List.concat
-  @@ int_product_tests
-  @@ fun (module P) ->
-  unit_sum_tests
-  @@ fun (module S) ->
+  @@ product_tests
+  @@ fun (module Product) ->
+  sum_tests
+  @@ fun (module Sum) ->
   flavoured_test_case_2
-    (module P)
-    (module S)
+    (module Product)
+    (module Sum)
     "select"
-    (test_product_select (module P) (module S))
+    (test_product_select (module Product.Make) (module Sum.Make))
 ;;
 
 let product_fold_tests =
-  int_product_tests
+  product_tests
   @@ fun (module P) ->
-  flavoured_test_case (module P) "fold" (test_product_fold (module P))
+  flavoured_test_case (module P) "fold" (test_product_fold (module P.Make))
 ;;
 
 let product_tests = product_select_tests @ product_fold_tests
 
 let sum_tests =
-  unit_sum_tests
-  @@ fun (module S) -> flavoured_test_case (module S) "tag" (test_sum_tag (module S))
+  sum_tests
+  @@ fun (module S) -> flavoured_test_case (module S) "tag" (test_sum_tag (module S.Make))
 ;;
 
 let () = run "adts" [ "product", product_tests; "sum", sum_tests ]
