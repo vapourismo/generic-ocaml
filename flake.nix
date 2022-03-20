73e1e78d4de9dc1ef3d0f314ae41ed6561220a73
Ole Krüger <ole@vprsm.de>
2022-03-20 14:10:21 +0000

    Add Nix Flake

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "generic-ocaml";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+    flake-utils.url = github:numtide/flake-utils;
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat }: flake-utils.lib.eachDefaultSystem (system:
+    with import nixpkgs { inherit system; };
+
+    let
+      ocamlPackages = ocaml-ng.ocamlPackages_4_13;
+    in
+    {
+      defaultPackage = ocamlPackages.buildDunePackage {
+        pname = "ppx_deriving";
+        version = "0.0.0";
+
+        useDune2 = true;
+
+        src = self;
+
+        buildInputs = with ocamlPackages; [
+          ppxlib
+        ];
+      };
+
+      devShell = mkShell {
+        nativeBuildInputs = with ocamlPackages; [
+          ocaml
+          ocaml-lsp
+          ocamlformat
+          findlib
+          dune_2
+          utop
+          nixpkgs-fmt
+          inotify-tools
+        ];
+
+        buildInputs = self.defaultPackage.${system}.buildInputs;
+      };
+    }
+  );
+}
