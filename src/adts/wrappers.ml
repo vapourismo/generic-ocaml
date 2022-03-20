1e138ed09727ddd53881bb3b6a124e3a1fb1ddb4
Ole Krüger <ole@vprsm.de>
2022-03-20 14:26:47 +0000

    Incorporate signatures into wrapper definitions

--- src/adts/wrappers.ml
+++ src/adts/wrappers.ml
@@ -2,25 +2,24 @@ module type S = sig
   type _ t
 end
 
-module Unit = struct
+module Unit : S with type 'a t = unit = struct
   type 'a t = unit
 end
 
-module Identity = struct
+module Identity : S with type 'a t = 'a = struct
   type 'a t = 'a
 end
 
-module List = struct
+module List : S with type 'a t = 'a list = struct
   type 'a t = 'a list
 end
 
-module Array = struct
+module Array : S with type 'a t = 'a array = struct
   type 'a t = 'a array
 end
 
 module To (T : sig
   type t
-end) =
-struct
+end) : S with type 'a t = 'a -> T.t = struct
   type 'a t = 'a -> T.t
 end
